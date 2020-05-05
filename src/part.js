0d9c0dc99f43e874af7b2b3c6b0844230265c461
Suzie Q <suzieqwolf@gmail.com>
2020-05-04 19:49:30 -0600

    Part class complete.

--- src/part.js
+++ src/part.js
@@ -1,6 +1,43 @@
 class Part {
-
+    constructor(type) {
+        // this.type = type;
+        this.validTypes = [
+            'wheel', 
+            'yoke', 
+            'axle', 
+            'ox',
+            undefined,
+        ];
+        this.type = this.validTypes.includes(type) ? type:undefined;
+            // valid types are 'wheel', 'yoke', 'axle', 'ox'
+            // this.type = this.validTypes.includes(type) ? type:undefined;
+        this.broken = false;
+            //not (boolean) broken by default
+        //this.hasBeenRepaired = undefined;
+    }
+    break() {
+        this.broken = true;
+    }
+        //method break(): changes this.broken (boolean) to true, no perameter
+    repair() {
+        this.broken = true !== this.hasBeenRepaired ? false:true;
+        this.hasBeenRepaired = true;
+            // if: this.broken is true AND this.hasBeenRepaired is true
+                // this.broken = true 
+            // else: this.broken = false 
+    }
+        //method repair(): changes this.broken (boolean) from false to true
+            //this.hasBeenRepaired (boolean) defined as true
 }
+
+
+
+
 
 module.exports = Part;
 // npm test test/part-test.js
+
+
+// from spaceport/src/part.js
+        // this.validType = ['landing gear', 'shell', 'hyperdrive', 'computer', 'life support', undefined]
+        // this.type = this.validType.includes(object.type) ? object.type : undefined;
