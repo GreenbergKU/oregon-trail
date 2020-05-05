bd884123c9de5eaada0e4c9bf89f5b71b8961d15
Suzie Q <suzieqwolf@gmail.com>
2020-05-05 02:23:45 -0600

    Creates duplicate class files with commented code.

--- src/classOregonTrail-NOTES/gearNotes.js
+++ src/classOregonTrail-NOTES/gearNotes.js
@@ -7,6 +7,9 @@ class Gear {
             undefined,
         ];
         this.type = this.validType.includes(type) ? type : undefined; 
+        // from spaceport/src/part.js
+            // this.validType = ['landing gear', 'shell', 'hyperdrive', 'computer', 'life support', undefined]
+            //this.type = this.validType.includes(object.type) ? object.type : undefined;
         this.quantity = quantity;   
     }
 }
--- test/settler-test.js
+++ test/settler-test.js
@@ -1,5 +1,5 @@
 var assert = require('chai').assert;
-var Settler = require('../src/settler.js');
+var Settler = require('../src/classOregonTrail-NOTES/settlerNotes.js');
 
 describe('Settler', function() {
   it('should have a name', function() {
